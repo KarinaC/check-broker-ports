e2674357e2994d674e991fb4820d19a6c7c0cb55
karmukis <karmukis@gmail.com>
2018-04-12 12:17:14 -0300

    Checks the ip and ports for the kafka brokers by using nslookup

--- README.md
+++ README.md
@@ -7,15 +7,9 @@ Used to check the broker ports on use
 
 ### Prerequisites
 
-Python3 is recommended 
-
 
 ### How to use
 
-
-```
-python3 port-check-v14.py --ports 10010 --marathon localhost:18082
-```
 
 ## Authors
 
---
+++ kafka-port-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+####################################################################
+# This scripts checks the IP and PORT being use by the kafka brokers
+####################################################################
+
+DATE=`date '+%d-%m-%Y %H:%M'`
+
+################
+# Scripts begins
+################
+
+printf "\n"
+echo "################################################"
+echo "Checking ip and port for date: $DATE"
+echo "################################################"
+
+for n in `seq 0 2`; 
+   do 
+       printf "\n"
+       echo This is the IP and PORT for broker-$n.kafka.mesos
+       nslookup -type=A broker-$n.kafka.mesos | grep 'Address: ' | awk -F':' '{print "Ip:  " $2}'
+       nslookup -type=SRV _broker-$n._tcp.kafka.mesos | grep service | awk -F' ' '{print "Port: " $6}'
+       printf "\n\n"
+ done
+
